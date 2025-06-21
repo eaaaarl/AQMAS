21cac2fe24ada6a684a3b6fe811ca11a07c22000
eaaaarl <dev.earl00@gmail.com>
2025-06-21 13:19:47 +0800

    refactor(mobile-queue-app/service): implement clean architecture and component separation

---
+++ mobile/mobile-queue-app/features/service/components/index.ts
@@ -0,0 +1,9 @@
+export { PaginationControls } from './PaginationControl';
+export { PrintReceiptButton } from './PrintReceiptButton';
+export { ServiceGrid } from './ServiceGrid';
+export { ServiceItem } from './ServiceItem';
+export { ServiceLayout } from './ServiceLayout';
+export { ServiceModals } from './ServiceModals';
+export { ServiceNavigation } from './ServiceNavigation';
+export { ServicePageContainer } from './ServicePageContainer';
+export { default as SurveyButton } from './SurveyButton';
